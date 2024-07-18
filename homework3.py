eabc002e0df5a4eebba2b4bb6c8264bbd6704655
Vladimir120442 <vgs2002@yandex.ru>
2024-07-18 17:08:12 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Владимир'
+print('Name:', name)
+age = 69
+print('Age:', age, type(age))
+age = (age+3)
+print('New age:', age, type(age))
+is_student = True
+print('Is_student:', is_student, type(is_student))
