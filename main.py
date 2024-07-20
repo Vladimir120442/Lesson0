7386cb75cdea46ce6917fe4c0491ce61b8456067
Vladimir120442 <vgs2002@yandex.ru>
2024-07-21 00:09:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+             #1st program
+print(9**0.5*5)
+             #2nd program
+print(9.9>9.8 and 1000 != 1000.1)
+             #3rd program
+a = 1234
+print(a%1000//10)
+b = 5678
+print(b%1000//10)
+             #4th program"
+a = 13.42
+a1 = print(a//1)#Целая часть 1-го числа
+a2 = print(round((a%1),2))#Дробная часть 1-го числа
+b = 42.13
+b1 = print(b//1)#Целая часть 2-го числа
+b2 = print(round((b%1),2))#Дробная часть 2-го числа
+print(a1 == b2 or b1 == a2)
