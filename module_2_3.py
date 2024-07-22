b48f83af28975a943043873e67420974ac96c302
Vladimir120442 <vgs2002@yandex.ru>
2024-07-22 23:38:08 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+in_value = int(0)
+while in_value <= len(my_list) and my_list[in_value] >= 0:
+    if my_list[in_value] > 0:
+        print(my_list[in_value])
+    in_value = in_value + 1
+
+
+
+
+
+
