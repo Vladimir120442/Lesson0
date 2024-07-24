97a949cd7984dc4e9f58d20c6d6c56b834d7865d
Vladimir120442 <vgs2002@yandex.ru>
2024-07-24 18:14:05 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,10 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 in_value = int(0)
-while in_value <= len(my_list) and my_list[in_value] >= 0:
-    if my_list[in_value] > 0:
+while in_value < len(my_list):
+    if(my_list[in_value]) > 0:
         print(my_list[in_value])
+    if (my_list[in_value]) < 0:
+        break
     in_value = in_value + 1
 
 
