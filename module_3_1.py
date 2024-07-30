c8fdaa9bbccaa8f610aa1fd1eb55a54b878186d9
Vladimir120442 <vgs2002@yandex.ru>
2024-07-30 15:10:17 +0300

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -1,31 +1,30 @@
 calls = 0
-def count_calls(calls1):
+def count_calls():
     global calls
     calls = calls + 1
-    print(calls)
 
-def string_info():
-    count_calls(calls)
-    string = 'Программирование'
+def string_info(string):
+    count_calls()
     string = (len(string), string.upper(), string.lower())
     print(string)
 
-def is_contains():
-    count_calls(calls)
-    string = ('Небо')
+def is_contains(string, list_to_search):
+    count_calls()
     string = string.upper()
-    list_to_search = ['Буря', 'мглою', 'небо', 'кроет']
     list_to_search = [k.upper() for k in list_to_search]
-    i = (string)
     l = False
     for i in list_to_search:
         if (i == string):
             l = True
-            break
     print(l)
 
-string_info()
-is_contains()
+string_info('Чем больше я знаю, тем больше понимаю, что не знаю ничего')
+
+is_contains('НеБо', ('Буря', 'мглою', 'небо', 'кроет'))
+
+print(calls)
+
+
 
 
 
