47bc9ff06c52af7c9724dd9ff0480efe33567dbe
Vladimir120442 <vgs2002@yandex.ru>
2024-08-07 22:24:30 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as divide_fake
+from true_math import divide as divide_true
+
+print(divide_fake(34,0))
+print(divide_fake(9,42))
+print(divide_true(69,14))
+print(divide_true(0,0))
