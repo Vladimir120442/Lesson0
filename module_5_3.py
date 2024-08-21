37ccf595763423dda48f6ab4821a7915110a0ec4
Vladimir120442 <vgs2002@yandex.ru>
2024-08-21 23:04:35 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -17,34 +17,50 @@ class House:
     def __len__(self):
         return self.numbers_of_floors
 
+    def __check__(self, other):
+        other = other.numbers_of_floors
+        if not isinstance(other, int) and not isinstance(other, House):
+            print('Переменная не относится ни к типу "int", ни к классу "House"')
+
     def __eq__(self, other):
+        self.__check__(other)
         return self.numbers_of_floors == other.numbers_of_floors
 
     def __lt__(self, other):
+        self.__check__(other)
         return self.numbers_of_floors < other.numbers_of_floors
 
     def __le__(self, other):
+        self.__check__(other)
         return self.numbers_of_floors <= other.numbers_of_floors
 
     def __gt__(self, other):
+        self.__check__(other)
         return self.numbers_of_floors > other.numbers_of_floors
 
     def __ge__(self, other):
+        self.__check__(other)
         return not self.__eq__(other)
 
     def __add__(self, value):
+        if not isinstance(value, int):
+            print('Переменная не относится к типу "int"')
         self.numbers_of_floors = self.numbers_of_floors + value
         return self
 
     def __radd__(self, value):
+        if not isinstance(value, int):
+            print('Переменная не относится к типу "int"')
         return self.__add__(value)
 
     def __iadd__(self, value):
+        if not isinstance(value, int):
+            print('Переменная не относится к типу "int"')
         return self.__add__(value)
 
 
-h1 = House('ЖК "Три пальмы"', 5)
-h2 = House('ЖК "Рублевка"', 3)
+#  h1 = House('ЖК "Три пальмы"', 5)
+#  h2 = House('ЖК "Рублевка"', 3)
 
 h1 = House('ЖК "Эльбрус"', 10)
 h2 = House('ЖК "Акация"', 20)
