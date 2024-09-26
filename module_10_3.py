975907f293db87aa0a45d88b15e817757257c130
Vladimir120442 <vgs2002@yandex.ru>
2024-09-26 22:27:51 +0300

    Add files via upload

--- module_10_3.py
+++ module_10_3.py
@@ -1,5 +1,4 @@
 import threading
-from threading import Thread, Lock
 import time
 import random
 
@@ -12,30 +11,40 @@ class Bank:
     def deposit(self):
         for _ in range(100):
             amount_deposit = random.randint(50, 500)  # Генерация суммы пополнения баланса
-            with self.lock:  # Блокировка потока снятия средств
-                if self.balance <= 500 and self.lock.locked():  # Проверка доступности баланса
-                    #self.lock.release()
-                    self.balance += amount_deposit
-                    print(f'Пополнение на сумму: {amount_deposit}. Баланс: {self.balance} у.е.')
+            self.lock.acquire()  # Начальная блокировка баланса
+
+            try:    # Для проверки и исключения "гонки" потоков
+                self.balance += amount_deposit
+                print(f'Пополнение на сумму: {amount_deposit}. Баланс: {self.balance} у.е.\n')
+                if self.balance >= 500:  # Проверка баланса (необязательно)
+                    pass
+            finally:
+                self.lock.release()  # Снятие блокировки баланса
+
             time.sleep(0.001)
 
     def take(self):
         for _ in range(100):
             withdrawal_deposit = random.randint(50, 500)  # Генерация суммы снятия средств
-            print(f'Запрос снятия средств на сумму на {withdrawal_deposit}')
-            with self.lock:                                     # Блокировка потока пополнения средств
+            print(f'Запрос снятия средств на сумму {withdrawal_deposit}\n')
+            self.lock.acquire()  # Начальная блокировка баланса
+
+            try:    # Для проверки и исключения "гонки" потоков
                 if withdrawal_deposit <= self.balance:
                     self.balance -= withdrawal_deposit
                     print(f'Снятие средств на сумму: {withdrawal_deposit}. Баланс: {self.balance} у.е.')
                 else:
                     print('Запрос отклонён, недостаточно средств')
-            #print(f'Запрос снятия средств на сумму на {withdrawal_deposit}')
+            finally:
+                self.lock.release()  # Снятие блокировки баланса
+
             time.sleep(0.001)
 
 
 bk = Bank()
 
-# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+# Т.к. методы принимают только self, никаких атрибутов нет,
+# то в потоки следует передавать сам объект (экземпляр) класса Bank()
 th1 = threading.Thread(target=Bank.deposit, args=(bk,))
 th2 = threading.Thread(target=Bank.take, args=(bk,))
 
@@ -46,3 +55,4 @@ th1.join()
 th2.join()
 
 print(f'Итоговый баланс: {bk.balance} у.е.')
+
